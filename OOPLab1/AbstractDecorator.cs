b2525f94751a73bb74e6222fa681577d26080cdb
melalex <melalex490@gmail.com>
2016-06-20 15:32:01 +0300

    Project template

---
+++ OOPLab1/AbstractDecorator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    abstract class Product
+    {
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    abstract class Decorator : Product
+    {
+        protected Product product; 
+        public override string ToString()
+        {
+            return product.ToString();
+        }
+    }
+}
